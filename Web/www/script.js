20f4e1a92a1b94a7ddad03a467a0d2dd0071c489
skamos <skamosbllurp@gmail.com>
2022-04-20 15:55:13 +0200

    modif en plus

--- Web/www/script.js
+++ Web/www/script.js
@@ -1,5 +1,10 @@
 var nom = document.getElementById('nom')
+var nom_statu = document.getElementById('nom-statu')
+
 var blabla = function() {
     nom.value = "blabla"
+    nom_statu.classList.remove('hide')
+    nom_statu.classList.add('valide')
 }
 nom.addEventListener('focusout', blabla)
+document.getElementById('nom').appendChild()
